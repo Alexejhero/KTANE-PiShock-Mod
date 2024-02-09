6f437ff7aaab396e9c248da37054e8c2fa7a7701
Alexejhero <32238504+Alexejhero@users.noreply.github.com>
2024-02-09 18:21:30 +0200

    alarm clock delay (idk if it works)

---
+++ KtaneMOD/AlarmClockHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using Assets.Scripts.Props;
+using HarmonyLib;
+using UnityEngine;
+
+namespace KtaneMOD;
+
+[HarmonyPatch]
+internal static class AlarmClockHandler
+{
+    private static bool _disablePatch;
+
+    [HarmonyPatch(typeof(AlarmClock), nameof(AlarmClock.TurnOn))]
+    [HarmonyPrefix]
+    private static bool DelayAlarmClock(AlarmClock __instance)
+    {
+        if (_disablePatch) return true;
+
+        PiShock.Self.AlarmClockOn();
+        Plugin.Instance.StartCoroutine(CoTurnOn());
+
+        return false;
+
+        IEnumerator CoTurnOn()
+        {
+            yield return new WaitForSeconds(OnlineConfig.PresendOffset);
+
+            _disablePatch = true;
+            __instance.TurnOn();
+            _disablePatch = false;
+        }
+    }
+}
